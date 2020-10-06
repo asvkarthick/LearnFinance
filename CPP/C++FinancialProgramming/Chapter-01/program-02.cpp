0ce43d867f2ebf415091e9246dae7c0fcf476ed2
Karthick Kumaran <asvkarthick@gmail.com>
2020-10-06 13:44:01 -0700

    Program to compute compound interest
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ CPP/C++FinancialProgramming/Chapter-01/program-02.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+
+class CompoundInterest
+{
+public:
+    CompoundInterest(double rate);
+    CompoundInterest(const CompoundInterest &c);
+    CompoundInterest &operator =(const CompoundInterest &c);
+    ~CompoundInterest();
+
+    double multiplePeriod(double value, int numPeriods);
+    double continuousCompounding(double value, int numPeriods);
+private:
+    double mRate;
+};
+
+CompoundInterest::CompoundInterest(double rate) : mRate(rate)
+{
+}
+
+CompoundInterest::CompoundInterest(const CompoundInterest &c) : mRate(c.mRate)
+{
+}
+
+CompoundInterest &CompoundInterest::operator =(const CompoundInterest &c)
+{
+    if (&c != this)
+        this->mRate = c.mRate;
+    return *this;
+}
+
+CompoundInterest::~CompoundInterest()
+{
+}
+
+double CompoundInterest::multiplePeriod(double value, int numPeriods)
+{
+    return value * pow(1 + mRate, numPeriods);
+}
+
+double CompoundInterest::continuousCompounding(double value, int numPeriods)
+{
+    return value * exp(mRate * numPeriods);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4) {
+        std::cerr << "Usage : " << argv[0] << " <interest rate> <principal> <num periods>" << std::endl;
+        return -1;
+    }
+
+    double rate = atof(argv[1]);
+    double value = atof(argv[2]);
+    double numPeriods = atoi(argv[3]);
+
+    CompoundInterest c(rate);
+    double result = c.multiplePeriod(value, numPeriods);
+    double contResult = c.continuousCompounding(value, numPeriods);
+
+    std::cout << "Future value of discrete compounding = " << result << std::endl;
+    std::cout << "Future value of continuous compounding = " << contResult << std::endl;
+
+    return 0;
+}
